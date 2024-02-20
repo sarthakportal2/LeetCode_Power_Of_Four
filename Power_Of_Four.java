1e2b40482044ff726db742c387974de125544647
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-02-20 22:43:50 +0530

    Power_Of_Four.java

--- Power_Of_Four.java
+++ Power_Of_Four.java
@@ -1,8 +1,7 @@
-public class Solution {
-    public bool IsPowerOfFour(int n) {
-        while (n>=4){
-            if (n%4!=0)return false;
-            n=n/4;
-        }return n==1;
-    }
-}
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        //Optimal solutionT(C(n))==(O(log(n))) and S(C(n))==O(1) as it requires non addition space
+        while (n>=4){//iterating number till eqaul to 4
+            if (n%4!=0)return false;//checking if number divisbilty by 4  true and printing false
+            n=n/4;//dividing numebr by 4 
+        }return n==1;}}//printing true and false
